2ec60e4125db0060a876f3b1ace5569da43bfd58
Guillem Parrado <guillempado@gmail.com>
2022-01-11 15:36:27 +0100

    Tasca 2: Apartat Universidad template

---
+++ Tasca_2_Universidad.sql
@@ -0,0 +1,59 @@
+
+-- Primer apartat
+
+-- 1
+
+-- 2
+
+-- 3
+
+-- 4
+
+-- 5
+
+-- 6
+
+-- 7
+
+-- 8
+
+-- 9
+
+-- Segon apartat: LEFT JOIN i RIGHT JOIN
+
+-- 1
+
+-- 2
+
+-- 3
+
+-- 4
+
+-- 5
+
+-- 6
+
+-- Tercer apartat: Consultes resum
+
+-- 1
+
+-- 2
+
+-- 3
+
+-- 4
+
+-- 5
+
+-- 6
+
+-- 7
+
+-- 8
+
+-- 9
+
+-- 10
+
+-- 11
+
